5af6def32c6e9273779066cb9dbf644306e0fb24
Hans Raaf <hara@oderwat.de>
2017-01-07 20:40:51 +0100

    Config default changes
    - Changed acceleration defaults to more sane values.
    - Enabled LINEAR Autoleveling (for testing stuff. If you use a real
    sensor you need to specify the offset and should enable Z_SAFE_HOMING!)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -493,7 +493,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,  100, 400,90}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,  100, 400, 95}
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -508,7 +508,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_MAX_ACCELERATION      {3000,3000,1000,5000}
+#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 100, 10000 }
 /**
  * Default Acceleration (change/s) change = mm/s
  * Override with M204
@@ -517,9 +517,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          650     // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -794,7 +794,7 @@
  *       test on.
  */
 //#define AUTO_BED_LEVELING_3POINT
-//#define AUTO_BED_LEVELING_LINEAR
+#define AUTO_BED_LEVELING_LINEAR
 //#define AUTO_BED_LEVELING_BILINEAR
 //#define MESH_BED_LEVELING
 //#define AUTO_BED_LEVELING_UBL
